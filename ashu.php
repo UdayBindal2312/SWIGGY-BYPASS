e06015b01374504895407ddbd4e46aa4d437b3e6
Uday Bindal <72090728+UdayBindal2312@users.noreply.github.com>
2021-09-18 13:03:04 +0530

    Add files via upload

---
+++ ashu.php
@@ -0,0 +1,38 @@
+ <!DOCTYPE html>
+
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<meta name="" content="">
+<script src="/cdn-cgi/apps/head/f62yV6a-2Y2i83dMGY1WyoLV8xM.js"></script><link rel="stylesheet" href="new.css" type="text/css" media="all" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<title>Swiggy game Bypass Script</title>
+</head>
+<body bgcolor="yellow"><script>!function(a,b,c,d,e,f){a.ddjskey=e;a.ddoptions=f||null;var m=b.createElement(c),n=b.getElementsByTagName(c)[0];m.async=1,m.src=d,n.parentNode.insertBefore(m,n)}(window,document,"script","https://js.datadome.co/tags.js","9394D1E5A8EA027C47D8E12AB1CEE3", {"ajaxListenerPath": true});</script>
+<center>
+<form action=https://techssing.com/sw/ method="GET">
+<h1 class="heading">Swiggy Game Score Script</h1>
+<div class="input">
+<input type="text" name="mo" class="text" placeholder="Enter Number" required />
+</div>
+<button type="submit" class="submit" value="SUBMIT" name="submit">Sent Otp</button></form><br>
+<a href="https://telegram.dog/Oet_Official" target="_blank">
+<div class="telegram">
+<i class="fa fa-telegram" style="color:#fff; font-size: 4rem"></i><p>Join Telegram Channel</p>
+</div>
+</a>
+<h5> ads Area</h5>
+<div class="ads">
+
+
+<div id="M644260ScriptRootC1193979">
+</div>
+<script src="https://jsc.mgid.com/t/e/techssing.com.1193979.js" async type="1ef0014505f9ffcfbe9cb76c-text/javascript">
+</script>
+
+
+</div>
+</center>
+<script src="/cdn-cgi/scripts/7d0fa10a/cloudflare-static/rocket-loader.min.js" data-cf-settings="1ef0014505f9ffcfbe9cb76c-|49" defer=""></script></body>
+</html>
